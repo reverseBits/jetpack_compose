004b0a36b12d0ea41a6b1889d8924f3f9ac4abe9
Dharmesh Dabhi <dharmeshldabhi@gmail.com>
2024-09-04 19:20:09 +0530

    Side-effects

--- app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
@@ -4,44 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            JetpackComposeDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            FetchDataFromAPI()
+            RememberCoroutineScopeExample()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackComposeDemoTheme {
-        Greeting("Android")
+    FetchDataFromAPI()
+    RememberCoroutineScopeExample()
+}
+
+// LaunchedEffect example
+@Composable
+fun FetchDataFromAPI() {
+    var data by remember { mutableStateOf("Loading..") }
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        data = "Data fetched successfully"
+    }
+    
+    Text(text = data)
+}
+
+// rememberCoroutineScope example
+@Composable
+fun RememberCoroutineScopeExample() {
+    var result by remember { mutableStateOf("Tap me") }
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = result)
+        Button(onClick = {
+            scope.launch {
+                result = "Task Completed!"
+            }
+        }) {
+            Text(text = "Start Task")
+        }
     }
 }
+
+// rememberC
