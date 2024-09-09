62cdee0f9334eaeda2d2a11ba5b1b1680a7c76e0
Vidhi Patel <vidhi.patel@revversebits.tech>
2024-09-09 10:45:19 +0530

    Update Code of Side-effect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
--- app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
@@ -78,5 +78,3 @@ fun RememberCoroutineScopeExample() {
         }
     }
 }
-
-// rememberC
