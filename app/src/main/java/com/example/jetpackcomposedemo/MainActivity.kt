dd5618d8a8dfea00980ef633dc8b2e6a008134bb
Rakesh Lakum <rakesh.lakum@reversebits.tech>
2024-09-16 12:06:01 +0530

    Navigation Type safe implemented with data parsing and nested Navigation

--- app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeDemoTheme {
-//               AppNavigation()
+               AppNavigation()
                 AppNavigationNested()
             }
         }
